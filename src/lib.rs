5c970769a6802bdde34ff2680e93e4eff69a2133
bhmerchant@gmail.com <bhmerchant@gmail.com>
2021-01-05 04:02:03 -0800

    adding functionality for enums

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,12 @@
 mod maps;
+use crate::maps::map_struct;
 use quote::quote;
 use std::error::Error;
 use std::iter::Iterator;
 use syn::{
-    parse_macro_input, Attribute, Data, DataEnum, DataStruct, DeriveInput, Fields, FieldsNamed,
-    Generics, Ident, Meta, NestedMeta, Type, Variant,
+    parse_macro_input, Attribute, Data, DataEnum, DataStruct, DeriveInput, Field, Fields,
+    FieldsNamed, FieldsUnnamed, Generics, Ident, Meta, NestedMeta, Type, Variant,
 };
-
-fn skip_field(attrs: &[Attribute]) -> bool {
-    attrs.iter().any(|attr| match attr.parse_meta() {
-        Ok(meta) => match meta {
-            Meta::List(ml) => {
-                ml.path.is_ident("serde")
-                    && ml.nested.iter().any(|nm| match nm {
-                        NestedMeta::Meta(inner) => {
-                            inner.path().is_ident("skip")
-                                | inner.path().is_ident("skip_serialize")
-                                | inner.path().is_ident("skip_deserialize")
-                        }
-                        _ => false,
-                    })
-            }
-            _ => false,
-        },
-        _ => false,
-    })
-}
 
 fn gen_struct_impl(
     id: Ident,
@@ -33,27 +14,22 @@ fn gen_struct_impl(
     nfs: FieldsNamed,
 ) -> Result<proc_macro2::TokenStream, Box<dyn Error>> {
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
-    let id_string: String = format!("{}", id.to_string());
+    let struct_id_string: String = format!("{}", id.to_string());
 
-    let mut fids: Vec<Ident> = vec![];
-    let mut fid_strings: Vec<String> = vec![];
-    let mut fschemas: Vec<proc_macro2::TokenStream> = vec![];
-    let mut fpositions: Vec<usize> = vec![];
-
-    for (pos, nf) in nfs.named.iter().enumerate() {
-        let syn::Field {
-            ident, ty, attrs, ..
-        } = nf;
-        if !skip_field(attrs) {
-            let id = ident.as_ref().unwrap().clone();
-            let id_str = id.to_string();
-            let schema = maps::map_ty(&id_str, ty);
-            fids.push(id);
-            fschemas.push(schema?);
-            fid_strings.push(id_str);
-            fpositions.push(pos);
-        }
-    }
+    let schema = map_struct(
+        &struct_id_string,
+        None,
+        nfs.iter()
+            .filter_map(
+                |Field {
+                     attrs,
+                     ident: Some(id),
+                     ty,
+                     ..
+                 }| if !marked_skip {Some((&id, &ty))} else { None }
+            )
+            .collect::<Vec<(&Ident, &Type)>>(),
+    );
 
     Ok(quote!(
         impl#impl_generics avro_rs::schema_gen::Schematized for #id #ty_generics #where_clause {
@@ -61,40 +37,23 @@ fn gen_struct_impl(
             fn schematize(parent_namespace: Option<std::string::String>) -> avro_rs::schema::Schema {
                 let id_string = std::string::String::from(#id_string);
                 let this_namespace = parent_namespace.as_ref().map_or(id_string.clone(), |ns| std::format!("{}.{}", ns, &id_string));
-                avro_rs::schema::Schema::Record {
-                    name: avro_rs::schema::Name {
-                        name: std::string::String::from(#id_string),
-                        namespace: Some(this_namespace.clone()),
-                        aliases: None,
-                    },
-                    doc: None,
-                    fields: vec![#(
-                        avro_rs::schema::RecordField {
-                            name: std::string::String::from(#fid_strings),
-                            doc: None,
-                            default: None,
-                            schema: #fschemas,
-                            order: avro_rs::schema::RecordFieldOrder::Ascending,
-                            position: #fpositions,
-                        }
-                    ),*],
-                    lookup: {
-                        let mut r: std::collections::HashMap<String, usize> = std::collections::HashMap::new();
-                        #(r.insert(std::string::String::from(#fid_strings), #fpositions);)*
-                        r
-                    },
-                }
+                #schema
             }
         }
     ))
 }
 
 fn gen_enum_impl(
-    _id: Ident,
-    _generics: Generics,
-    _variants: Vec<Variant>,
+    enum_id: Ident,
+    generics: Generics,
+    variants: Vec<Variant>,
 ) -> Result<proc_macro2::TokenStream, Box<dyn Error>> {
-    unimplemented!()
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    let enum_id_string: String = format!("{}", enum_id.to_string());
+
+    map_enum()
+
+    Ok(quote!())
 }
 
 #[proc_macro_derive(Schematize)]
--- src/maps.rs
+++ src/maps.rs
@@ -1,18 +1,34 @@
+use crate::marked_skip;
 use proc_macro2::TokenStream;
 use quote::quote;
 use std::error::Error;
 use std::fmt;
 use std::fmt::{Debug, Display, Formatter};
-use syn::{GenericArgument, PathArguments, PathSegment};
+use syn::{
+    AngleBracketedGenericArguments, Attribute, Field, Fields, FieldsNamed, FieldsUnnamed,
+    GenericArgument, Ident, Meta, NestedMeta, Path, PathArguments, PathSegment, Type, TypeArray,
+    TypePath, TypeTuple, Variant,
+};
+use term::Attr;
 
 pub(crate) fn map_ty(namespace: &str, ty: &syn::Type) -> Result<TokenStream, Box<dyn Error>> {
     match ty {
-        syn::Type::Path(tp) => map_path(namespace, tp),
-        syn::Type::Array(ta) => {
-            let inner = map_ty(namespace, &ta.elem)?;
+        syn::Type::Path(TypePath { path, .. }) => map_path(namespace, path),
+        syn::Type::Array(TypeArray { elem, .. }) => {
+            let inner = map_ty(namespace, elem)?;
             Ok(quote!(avro_rs::schema::Schema::Array(Box::new(#inner))))
         }
-        _ => panic!("Schematize: cannot handle non-Path or Array syn::Type."),
+        syn::Type::Tuple(TypeTuple { elems, .. }) => Ok(map_tuple(
+            namespace,
+            None,
+            elems.iter().collect::<Vec<&Type>>(),
+        )?),
+        _ => {
+            panic!(
+                "Schematize: cannot handle non-Path or Array syn::Type. Received: {:?}",
+                ty
+            );
+        }
     }
 }
 
@@ -141,8 +157,7 @@ pub(crate) fn map_segs(namespace: &str, seg: &PathSegment) -> Result<TokenStream
         "Option" => map_option(&namespace, seg),
         "Vec" => map_vec(&namespace, seg),
         _ => match &seg.arguments {
-            PathArguments::AngleBracketed(angle_args) => {
-                let args = &angle_args.args;
+            PathArguments::AngleBracketed(AngleBracketedGenericArguments { args, .. }) => {
                 match args.len() {
                     1 => match args.first().unwrap() {
                         // Type::Path(TypePath { path, .. })
@@ -164,16 +179,15 @@ pub(crate) fn map_segs(namespace: &str, seg: &PathSegment) -> Result<TokenStream
     }
 }
 
-fn map_path(namespace: &str, tp: &syn::TypePath) -> Result<TokenStream, Box<dyn Error>> {
-    if tp.path.segments.is_empty() {
-        Err(Box::new(SchematizeError::EmptyPath(quote!(#tp))))
-    } else if let Some(id) = tp.path.get_ident() {
+fn map_path(namespace: &str, path: &Path) -> Result<TokenStream, Box<dyn Error>> {
+    if path.segments.is_empty() {
+        Err(Box::new(SchematizeError::EmptyPath(quote!(#path))))
+    } else if let Some(id) = path.get_ident() {
         map_id(namespace, id)
-    } else if tp.path.segments.len() == 1 {
-        map_segs(namespace, tp.path.segments.first().unwrap())
+    } else if path.segments.len() == 1 {
+        map_segs(namespace, path.segments.first().unwrap())
     } else {
-        let ids = tp
-            .path
+        let ids = path
             .segments
             .iter()
             .filter_map(|seg| match seg.arguments {
@@ -183,4 +197,205 @@ fn map_path(namespace: &str, tp: &syn::TypePath) -> Result<TokenStream, Box<dyn 
             .collect::<Vec<syn::Ident>>();
         Ok(quote!(#(#ids)::*))
     }
+}
+
+fn marked_skip(attrs: &[Attribute]) -> bool {
+    attrs.iter().any(|attr| match attr.parse_meta() {
+        Ok(meta) => match meta {
+            Meta::List(ml) => {
+                ml.path.is_ident("serde")
+                    && ml.nested.iter().any(|nm| match nm {
+                        NestedMeta::Meta(inner) => {
+                            inner.path().is_ident("skip")
+                                | inner.path().is_ident("skip_serialize")
+                                | inner.path().is_ident("skip_deserialize")
+                        }
+                        _ => false,
+                    })
+            }
+            _ => false,
+        },
+        _ => false,
+    })
+}
+
+pub(crate) fn map_tuple(
+    namespace: &str,
+    variant: Option<&str>,
+    tys: Vec<&Type>,
+) -> Result<TokenStream, Box<dyn Error>> {
+    let namespace = if let Some(v) = variant {
+        format!("{}.{}", namespace, v)
+    } else {
+        namespace.to_string()
+    };
+
+    let mut schemas: Vec<TokenStream> = Vec::with_capacity(tys.len());
+    for ty in tys.iter() {
+        schemas.push(map_ty(&namespace, ty)?)
+    }
+    let positions = (0..tys.len()).collect::<Vec<usize>>();
+
+    Ok(quote!(
+    avro_rs::schema::Schema::Record {
+        name: avro_rs::schema::Name {
+            name: std::format!("Tuple(#(#tys),*)"),
+            namespace: Some(std::string::String::from(#namespace)),
+            aliases: None,
+        },
+        doc: None,
+        fields: std::vec![#(
+            avro_rs::schema::RecordField {
+                name: #positions.to_string(),
+                doc: None,
+                default: None,
+                schema: #schemas,
+                order: avro_rs::schema::RecordFieldOrder::Ascending,
+                position: #positions,
+            }
+        ),*],
+        lookup: {
+            let mut r: std::collections::HashMap<String, usize> = std::collections::HashMap::new();
+            #(r.insert(#positions.to_string(), #positions);)*
+            r
+        },
+    }))
+}
+
+pub(crate) fn map_struct(
+    namespace: &str,
+    variant: Option<&str>,
+    fields: Vec<(&Ident, &Type)>,
+) -> Result<TokenStream, Box<dyn Error>> {
+    let namespace = if let Some(v) = variant {
+        format!("{}.{}", namespace, v)
+    } else {
+        namespace.to_string()
+    };
+
+    let mut fids: Vec<Ident> = Vec::with_capacity(fields.len());
+    let mut fid_strings: Vec<String> = Vec::with_capacity(fields.len());
+    let mut fschemas: Vec<proc_macro2::TokenStream> = Vec::with_capacity(fields.len());
+    let mut fpositions: Vec<usize> = Vec::with_capacity(fields.len());
+
+    for (pos, (fid, fty)) in fields.named.iter().enumerate() {
+        let syn::Field {
+            ident, ty, attrs, ..
+        } = nf;
+        let id = ident.as_ref().unwrap().clone();
+        let id_str = id.to_string();
+        let schema = map_ty(&namespace, &ty);
+        fids.push(id);
+        fschemas.push(schema?);
+        fid_strings.push(id_str);
+        fpositions.push(pos);
+    }
+
+    Ok(quote!(avro_rs::schema::Schema::Record {
+        name: avro_rs::schema::Name {
+            name: std::string::String::from(#id_string),
+            namespace: Some(this_namespace.clone()),
+            aliases: None,
+        },
+        doc: None,
+        fields: vec![#(
+            avro_rs::schema::RecordField {
+                name: std::string::String::from(#fid_strings),
+                doc: None,
+                default: None,
+                schema: #fschemas,
+                order: avro_rs::schema::RecordFieldOrder::Ascending,
+                position: #fpositions,
+            }
+        ),*],
+        lookup: {
+            let mut r: std::collections::HashMap<String, usize> = std::collections::HashMap::new();
+            #(r.insert(std::string::String::from(#fid_strings), #fpositions);)*
+            r
+        },
+    }))
+}
+
+fn map_union(variants: Vec<&Variant>) -> Result<TokenStream, Box<dyn Error>> {
+    let mut vids: Vec<Ident> = Vec::with_capacity(variants.len());
+    let mut vid_strings: Vec<String> = Vec::with_capacity(variants.len());
+    let mut vschemas: Vec<proc_macro2::TokenStream> = Vec::with_capacity(variants.len());
+    let mut vpositions: Vec<usize> = Vec::with_capacity(variants.len());
+
+    for (pos, &variant) in variants.iter().enumerate() {
+        let syn::Variant {
+            ident,
+            attrs,
+            fields,
+            ..
+        } = variant;
+        if !marked_skip(&attrs) {
+            let variant_id = ident.as_ref().unwrap().clone();
+            let variant_id_string = variant_id.to_string();
+            let namespace = format!("{}.{}", &enum_id_string, &variant_id_string);
+            let schema = match fields {
+                Fields::Named(FieldsNamed { named, .. }) => map_struct(
+                    &namespace,
+                    Some(variant_id),
+                    named
+                        .iter()
+                        .filter_map(
+                            |Field {
+                                 attrs,
+                                 ident: Some(id),
+                                 ty,
+                                 ..
+                             }| {
+                                if !marked_skip(attrs) {
+                                    Some((id, ty))
+                                } else {
+                                    None
+                                }
+                            },
+                        )
+                        .collect::<Vec<(&Ident, &Type)>>(),
+                ),
+                Fields::Unnamed(FieldsUnnamed { unnamed, .. }) => map_tuple(
+                    &namespace,
+                    Some(variant_id),
+                    unnamed
+                        .iter()
+                        .filter_map(
+                            |Field { attrs, ty, .. }| {
+                                if !marked_skip(attrs) {
+                                    Some(ty)
+                                } else {
+                                    None
+                                }
+                            },
+                        )
+                        .collect::<Vec<&Type>>(),
+                ),
+                Fields::Unit => quote!(avro_rs::schema::Schema::RecordField()),
+            };
+            vids.push(variant_id);
+            vschemas.push(schema?);
+            vid_strings.push(variant_id_string);
+            vpositions.push(pos);
+        }
+    }
+
+    Ok(quote!(avro_rs::scheme::Schema))
+}
+
+fn map_enum(
+    namespace: &str,
+    id: &Ident,
+    variants: Vec<String>,
+) -> Result<TokenStream, Box<dyn Error>> {
+    let id_string = id.to_string();
+    Ok(quote!(avro_rs::schema::Schema::Enum {
+        name: Name {
+            name: std::string::String::from(#id_string),
+            namespace: std::option::Option::Some(std::string::String::from(#namespace)),
+            aliases: None,
+        },
+        docs: None,
+        symbols: std::vec![#(std::string::String::from(#variants))*,],
+    }))
 }
